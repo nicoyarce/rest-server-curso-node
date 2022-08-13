5a39e7b3144c216901a1ebff95bbf5d195f3e19a
Nicolas Oyarce <nicoyarce@gmail.com>
2022-08-13 19:12:47 -0400

    Primer commit

---
+++ controllers/usuarios.js
@@ -0,0 +1,46 @@
+const { response, request } = require("express");
+const usuariosGet = (req = request, res = response) => {
+    const { string, page = 1 } = req.query;
+    res.json({
+        msg: "Get API - controlador",
+        string,
+        page,
+    });
+};
+
+const usuariosPost = (req, res = response) => {
+    const { nombre, edad } = req.body;
+    res.json({
+        msg: "Post API - controlador",
+        nombre,
+        edad,
+    });
+};
+
+const usuariosPut = (req, res = response) => {
+    const { id } = req.params;
+    res.json({
+        msg: "Put API - controlador",
+        id,
+    });
+};
+
+const usuariosPatch = (req, res = response) => {
+    res.json({
+        msg: "Patch API - controlador",
+    });
+};
+
+const usuariosDelete = (req, res = response) => {
+    res.json({
+        msg: "Delete API - controlador",
+    });
+};
+
+module.exports = {
+    usuariosGet,
+    usuariosPost,
+    usuariosPut,
+    usuariosPatch,
+    usuariosDelete,
+};
